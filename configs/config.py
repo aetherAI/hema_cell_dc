dbadccca2ae4542f6676409656888b11f506be66
jimmy <jimmy@aetherai.com>
2023-02-10 11:03:44 +0800

    Update code

--- configs/config.py
+++ configs/config.py
@@ -220,7 +220,7 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(
     policy='step',
